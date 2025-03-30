d4936ebab70427a8b8e7848d877718467783f578
Chayan Deb <the.supercd@gmail.com>
2025-03-30 16:30:55 +0530

    Fixed OpenGL core profile version number inside shader sources.

--- OpenGL-basics/src/drawTriangle.cpp
+++ OpenGL-basics/src/drawTriangle.cpp
@@ -7,7 +7,7 @@ static const int window_width = 400;
 static const int window_height = 400;
 
 // Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 460 core\n"
   "layout (location = 0) in vec3 aPos;\n"
   "void main()\n"
   "{\n"
@@ -15,7 +15,7 @@ const char* vertexShaderSource = "#version 330 core\n"
   "}\0";
 
 //Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 460 core\n"
   "out vec4 FragColor;\n"
   "void main()\n"
   "{\n"
